9d404df4b6bc5b87b8850bd509fd2edede67fb0b
Deivide Mateus <deivide.mateus@hotmail.com>
2019-05-27 16:45:40 -0300

    att pro firebase

--- calculador_imc/lib/login.dart
+++ calculador_imc/lib/login.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class Login extends StatelessWidget {
-  final formKey = GlobalKey<FormState>();
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
 
+enum FormType { login, register }
+
+class _LoginState extends State<Login> {
+  final formKey = GlobalKey<FormState>();
   String _email;
   String _password;
+  FormType _formType = FormType.login;
 
   bool validateAndSave() {
     final form = formKey.currentState;
@@ -19,13 +26,34 @@ class Login extends StatelessWidget {
   void validateAndSubmit() async {
     if (validateAndSave()) {
       try {
-        FirebaseUser user = await FirebaseAuth.instance
-            .signInWithEmailAndPassword(email: _email, password: _password);
-        print("Singned in: ${user.uid}");
+        if (_formType == FormType.login) {
+          FirebaseUser user = await FirebaseAuth.instance
+              .signInWithEmailAndPassword(email: _email, password: _password);
+          print("Singned in: ${user.uid}");
+        } else {
+          FirebaseUser user = await FirebaseAuth.instance
+              .createUserWithEmailAndPassword(
+                  email: _email, password: _password);
+          print("Usuário registrado: ${user.uid}");
+        }
       } catch (e) {
         print("Error: $e");
       }
     }
+  }
+
+  void moveToRegister() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.register;
+    });
+  }
+
+  void moveToLogin() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.login;
+    });
   }
 
   @override
@@ -34,49 +62,78 @@ class Login extends StatelessWidget {
       appBar: AppBar(
         title: Text("Entrar"),
         centerTitle: true,
-        actions: <Widget>[
-          FlatButton(
-            child: Text(
-              "CRIAR CONTA",
-              style: TextStyle(fontSize: 15.0),
-            ),
-            textColor: Colors.white,
-            onPressed: () {},
-          )
-        ],
       ),
       body: Form(
+        key: formKey,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: ListView(
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(hintText: "E-mail"),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) => value.isEmpty ? "Email inválido" : null,
-                onSaved: (value) => _email = value,
-              ),
-              SizedBox(
-                height: 16.0,
-              ),
-              TextFormField(
-                decoration: InputDecoration(hintText: "Senha"),
-                obscureText: true,
-                validator: (value) => value.isEmpty ? "Senha invalida" : null,
-                onSaved: (value) => _password = value,
-              ),
-              RaisedButton(
-                child: Text(
-                  "Login",
-                  style: TextStyle(fontSize: 20.0, color: Colors.white),
-                ),
-                color: Colors.green,
-                onPressed: validateAndSubmit,
-              )
-            ],
+            children: buildInputs() + buildSubmitButtons(),
           ),
         ),
       ),
     );
+  }
+
+  List<Widget> buildInputs() {
+    return [
+      TextFormField(
+        decoration: InputDecoration(hintText: "E-mail"),
+        keyboardType: TextInputType.emailAddress,
+        validator: (value) => value.isEmpty ? "Email inválido" : null,
+        onSaved: (value) => _email = value,
+      ),
+      SizedBox(
+        height: 16.0,
+      ),
+      TextFormField(
+        decoration: InputDecoration(hintText: "Senha"),
+        obscureText: true,
+        validator: (value) => value.isEmpty ? "Senha invalida" : null,
+        onSaved: (value) => _password = value,
+      ),
+    ];
+  }
+
+  List<Widget> buildSubmitButtons() {
+    if (_formType == FormType.login) {
+      return [
+        RaisedButton(
+          child: Text(
+            "Login",
+            style: TextStyle(fontSize: 20.0, color: Colors.white),
+          ),
+          color: Colors.green,
+          onPressed: validateAndSubmit,
+        ),
+        RaisedButton(
+          child: Text(
+            "Criar conta",
+            style: TextStyle(fontSize: 20.0, color: Colors.white),
+          ),
+          color: Colors.green,
+          onPressed: moveToRegister,
+        )
+      ];
+    } else {
+      return [
+        RaisedButton(
+          child: Text(
+            "Criar conta",
+            style: TextStyle(fontSize: 20.0, color: Colors.white),
+          ),
+          color: Colors.green,
+          onPressed: validateAndSubmit,
+        ),
+        RaisedButton(
+          child: Text(
+            "Possui uma conta? Faça Login",
+            style: TextStyle(fontSize: 20.0, color: Colors.white),
+          ),
+          color: Colors.green,
+          onPressed: moveToLogin,
+        )
+      ];
+    }
   }
 }
